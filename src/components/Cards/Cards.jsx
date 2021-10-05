e7505fd5b8c044d1a02b5745c093a1760f299e02
Harsh Rastogi <rastogiharsh04@gmail.com>
2021-10-05 21:11:05 +0530

    Cards Created

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,20 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import './card.css';
 
-const Cards = () => {
-  return <div></div>;
+const Cards = ({ title, children }) => {
+  return (
+    <div className="text-gray-600 p-8 space-y-2 text-center shadow-lg rounded-md card">
+      <h4 className="font-bold text-red-600 text-lg">{title}</h4>
+      <p className="">{children}</p>
+    </div>
+  );
+};
+
+Cards.propTypes = {
+  logo: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
 };
 
 export default Cards;
