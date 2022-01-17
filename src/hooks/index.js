04b25a22a4dc75e7f801beeddb8ad77024cbb146
theharshrastogi <rastogiharsh04@gmail.com>
2022-01-17 21:12:50 +0530

    useGoogleAuth Refactored

---
+++ src/hooks/index.js
@@ -0,0 +1,3 @@
+import useGoogleAuth from "./useGoogleAuth";
+
+export { useGoogleAuth };
--- src/hooks/useGoogleAuth.js
+++ src/hooks/useGoogleAuth.js
@@ -1,29 +1,34 @@
 /* eslint-disable no-undef */
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-export function useGoogleAuth(clientId, apiKey) {
+const SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+
+// Function Setup GoogleAuth Instance
+function setupGoogleAuth(config) {
+  return new Promise((resolve) => {
+    gapi.load("client:auth2", async () => {
+      await gapi.client.init(config);
+      resolve(gapi.auth2.getAuthInstance());
+    });
+  });
+}
+
+export default function useGoogleAuth(clientId, apiKey) {
   const [GoogleAuth, setGoogleAuth] = useState(null);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    function init() {
-      gapi.load('client:auth2', async () => {
-        await gapi.client.init({
-          scope: 'https://www.googleapis.com/auth/userinfo.email',
-          apiKey,
-          clientId,
-        });
+    const DEFAULT_CREDENTIALS = { scope: SCOPE, apiKey, clientId };
 
-        setGoogleAuth(gapi.auth2.getAuthInstance());
-      });
-    }
-
-    try {
-      init();
-    } catch (e) {
-      console.log(e);
-      setError(e);
-    }
+    (async () => {
+      try {
+        const GoogleAuth = await setupGoogleAuth(DEFAULT_CREDENTIALS);
+        setGoogleAuth(GoogleAuth);
+      } catch (e) {
+        console.err(e);
+        setError(e);
+      }
+    })();
   }, [clientId, apiKey]);
 
   return { GoogleAuth, error };
